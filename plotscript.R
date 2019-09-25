c69bf6cc19bb33ea0458a605236fe337f6319957
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-25 16:21:28 +0800

    revise the bound

--- plotscript.R
+++ plotscript.R
@@ -3,6 +3,36 @@ for (i in seq_len(length(HRA01.gr))) {
   plotGviz(coding.obj = HRA01.gr,
            i = i,
            title = '',
-           bounds = 10000,
+           bounds = 20000,
            path = '../HRA01/')
+}
+for (i in seq_len(length(HRA02.gr))) {
+  plotGviz(coding.obj = HRA02.gr,
+           i = i,
+           title = '',
+           bounds = 20000,
+           path = '../HRA02/')
+}
+for (i in seq_len(length(HRA03.gr))) {
+  plotGviz(coding.obj = HRA03.gr,
+           i = i,
+           title = '',
+           bounds = 20000,
+           path = '../HRA03/')
+}
+for (i in seq_len(length(HRA04.gr))) {
+  plotGviz(coding.obj = HRA04.gr,
+           i = i,
+           title = '',
+           bounds = 20000,
+           path = '../HRA04/')
+}
+
+
+for (i in seq_len(length(total.var))) {
+  plotGviz(coding.obj = total.var,
+           i = i,
+           title = '',
+           bounds = 20000,
+           path = '../total/')
 }
