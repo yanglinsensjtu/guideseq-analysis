2848350b60bc8622962e8d3af1e7ca995dd0f4a2
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-25 16:29:08 +0800

    duplicate remove

--- annotation.R
+++ annotation.R
@@ -1,6 +1,7 @@
 library(VariantAnnotation)
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
-total.var <- unique(locateVariants(total, txdb, AllVariants()))
-table(unique(total.var)$LOCATION)
+total.var <- locateVariants(total, txdb, AllVariants())
+total.var.re.Duplicate <- unique(granges(total.var))
+table(unique(total.var.re.Duplicate)$LOCATION)
 
