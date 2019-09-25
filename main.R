011e7f768fd0c9970901ded4e17fbe1275195310
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-25 14:54:04 +0800

    add analysis script

---
+++ main.R
@@ -0,0 +1,6 @@
+library(readxl)
+
+HRA01 <- read_excel('../6bp_identifiedOfftargets.xlsx', sheet = 'HRA01')
+HRA02 <- read_excel('../6bp_identifiedOfftargets.xlsx', sheet = 'HRA02')
+HRA03 <- read_excel('../6bp_identifiedOfftargets.xlsx', sheet = 'HRA03')
+HRA04 <- read_excel('../6bp_identifiedOfftargets.xlsx', sheet = 'HRA04')
