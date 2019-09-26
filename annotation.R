4f083d1bb10b92f6e1eb571ce9ea2aa4d88c869e
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-26 11:11:07 +0800

    fix

--- annotation.R
+++ annotation.R
@@ -3,5 +3,5 @@ library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 total.var <- locateVariants(total, txdb, AllVariants())
 total.var.re.Duplicate <- unique(granges(total.var))
-table(unique(total.var.re.Duplicate)$LOCATION)
+table(unique(total.var)$LOCATION)
 
